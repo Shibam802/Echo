d3e3574a1331a954c6d2df892ca4423583f7e8a2
Shibam802 <shibambanik0@gmail.com>
2025-10-22 23:14:24 +0530

    Initial commit - Echo

---
+++ models/post.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose');
+
+
+const postSchema = mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    },
+    data:{
+        type:Date,
+        default:Date.now
+    },
+    content:String,
+    likes:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    }
+    ]
+});
+
+
+/*const postSchema = mongoose.Schema({
+    postdata:String,
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    },
+    data:{
+        type:Date,
+        default:Date.now
+    }
+})*/
+
+module.exports=mongoose.model('post',postSchema);
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/testingthedatabase");
+
+/*const userSchema = mongoose.Schema({
+    username:String,
+    email:String,
+    age:Number,
+    posts:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'post'
+    }]
+})
+
+module.exports=mongoose.model('user',userSchema);*/
+
+
+
+const userSchema = mongoose.Schema({
+    username:String,
+    email:String,
+    age:Number,
+    name:String,
+    password:String,
+    posts:[{
+     type:mongoose.Schema.Types.ObjectId, ref:'post'
+    }]
+})
+
+module.exports=mongoose.model('user',userSchema)
